531beb68f415aeb700ec31fc912b6f250465319e
Dessewffy <88779121+Dessewffy@users.noreply.github.com>
2024-01-14 09:14:08 +0100

    Add files via upload

---
+++ test/hootel1_test.py
@@ -0,0 +1,52 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import pytest
+import allure
+class TestHootel(object):
+    def setup_method(self):
+        URL = 'http://hotel-v3.progmasters.hu/'
+        options = Options()
+        options.add_experimental_option("detach", True)
+        self.browser = webdriver.Chrome(options=options)
+
+        self.browser.get(URL)
+        self.browser.maximize_window()
+
+    def teardown_method(self):
+        self.browser.quit()
+
+    @pytest.mark.parametrize('email, password', [('hogap65094@zamaneta.com', '1234')])
+    @allure.title("Hootel Login")
+    @allure.description("A belépés tesztelése")
+    @allure.severity(allure.severity_level.TRIVIAL)
+    @allure.tag("login")
+    def test_login(self, email, password):
+        login_btn = self.browser.find_element(By.XPATH, '//a[@class="nav-link"]')
+        login_btn.click()
+
+        email_input = self.browser.find_element(By.ID, 'email')
+        email_input.send_keys(email)
+
+        password_input = self.browser.find_element(By.ID, 'password')
+        password_input.send_keys(password)
+
+        submit_btn = self.browser.find_element(By.NAME, 'submit')
+        submit_btn.click()
+        time.sleep(1)
+
+        logout_btn = self.browser.find_element(By.ID, 'logout-link')
+
+        assert logout_btn.text == "Kilépés"
+    def test_navigation(self):
+        lista = self.browser.find_element(By.CLASS_NAME, "btn-outline-primary")
+        assert lista.is_enabled()
+        lista.click()
+        time.sleep(1)
+
+        nav = self.browser.find_element(By.XPATH, '//a[text()="2"]')
+        assert nav.is_displayed()
+        nav.click()
+
+       
