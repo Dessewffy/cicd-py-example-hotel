863b2b0c5eeb980e6ee6a9d49833c3c5933e50a2
Dessewffy <88779121+Dessewffy@users.noreply.github.com>
2024-01-14 11:39:17 +0100

    Update test_homework3.py

--- test/test_homework3.py
+++ test/test_homework3.py
@@ -12,7 +12,7 @@ class TestHootel(object):
         URL = 'http://hotel-v3.progmasters.hu/'
         options = Options()
         options.add_experimental_option("detach", True)
-        options.add_argument('--headless')
+       # options.add_argument('--headless')
         self.driver = webdriver.Chrome(options=options)
         self.driver.get(URL)
         self.driver.maximize_window()
