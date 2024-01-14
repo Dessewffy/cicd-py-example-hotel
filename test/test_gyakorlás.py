9ee46d9549edb64656124ebf456f4adb87b9f2c8
Dessewffy <88779121+Dessewffy@users.noreply.github.com>
2024-01-14 11:03:07 +0100

    Update test_gyakorlás.py

--- test/test_gyakorlás.py
+++ test/test_gyakorlás.py
@@ -17,7 +17,7 @@ class TestHootel(object):
         options = Options()
         options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=options)
-
+        options.headless = False
         self.driver.get(URL)
         self.driver.maximize_window()
 
