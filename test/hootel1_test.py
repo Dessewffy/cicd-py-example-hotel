54887353a13d464a3a6a49dd20f98328e7b2f35e
Dessewffy <88779121+Dessewffy@users.noreply.github.com>
2024-01-14 11:38:41 +0100

    Update hootel1_test.py

--- test/hootel1_test.py
+++ test/hootel1_test.py
@@ -9,7 +9,7 @@ class TestHootel(object):
         URL = 'http://hotel-v3.progmasters.hu/'
         options = Options()
         options.add_experimental_option("detach", True)
-        options.add_argument('--headless')
+        #options.add_argument('--headless')
         self.browser = webdriver.Chrome(options=options)
 
         self.browser.get(URL)
