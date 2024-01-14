16dd3a4c43dcba398a1a9da7c0fb460b290cc590
Dessewffy <88779121+Dessewffy@users.noreply.github.com>
2024-01-14 12:12:17 +0100

    Update test_gyakorlás1.py

--- test/test_gyakorlás1.py
+++ test/test_gyakorlás1.py
@@ -34,11 +34,11 @@ class TestHootel(object):
 
     def test_login_and_logout(self):
         menu_toggle = WebDriverWait(self.browser, 5).until(
-            EC.element_to_be_clickable((By.XPATH, '//button[@class="navbar-toggler collapsed"]')))
+        EC.element_to_be_clickable((By.XPATH, '//button[@class="navbar-toggler collapsed"]')))
         menu_toggle.click()
 
         login_btn = WebDriverWait(self.browser, 5).until(
-            EC.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
+           EC.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
         login_btn.click()
 
         email_input = self.browser.find_element(By.ID, 'email')
