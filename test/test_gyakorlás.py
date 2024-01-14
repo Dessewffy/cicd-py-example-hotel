354721e7bbbaec1089344b3431d1c7e471c88d3c
Dessewffy <88779121+Dessewffy@users.noreply.github.com>
2024-01-14 11:56:08 +0100

    Update test_gyakorlás.py

--- test/test_gyakorlás.py
+++ test/test_gyakorlás.py
@@ -31,22 +31,25 @@ class TestHootel(object):
     @allure.severity(allure.severity_level.TRIVIAL)
     @allure.tag("login")
     def test_login(self, email, password):
-        login_btn = self.driver.find_element(By.XPATH, '//a[@class="nav-link"]')
+      menu_toggle = WebDriverWait(self.browser, 5).until(
+            EC.element_to_be_clickable((By.XPATH, '//button[@class="navbar-toggler collapsed"]')))
+        menu_toggle.click()
+
+        login_btn = WebDriverWait(self.browser, 5).until(
+            EC.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
         login_btn.click()
 
-        email_input = self.driver.find_element(By.ID, 'email')
+        email_input = self.browser.find_element(By.ID, 'email')
         email_input.send_keys(email)
 
-        password_input = self.driver.find_element(By.ID, 'password')
+        password_input = self.browser.find_element(By.ID, 'password')
         password_input.send_keys(password)
 
-        submit_btn = self.driver.find_element(By.NAME, 'submit')
+        submit_btn = self.browser.find_element(By.NAME, 'submit')
         submit_btn.click()
+        time.sleep(1)
 
-        logout_btn = WebDriverWait(self.driver, 5).until(
-            EC.presence_of_element_located((By.ID, 'logout-link'))
-        )
-        # logout_btn = self.driver.find_element(By.ID, 'logout-link')
+        logout_btn = self.browser.find_element(By.ID, 'logout-link')
 
         assert logout_btn.text == "Kilépés"
 
