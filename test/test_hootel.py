320c99494108b1563db2e165c40e7b204af7489d
Dessewffy <88779121+Dessewffy@users.noreply.github.com>
2024-01-14 11:59:02 +0100

    Update test_hootel.py

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -2,8 +2,10 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-import allure
 import pytest
+import allure
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestHootel(object):
@@ -25,7 +27,13 @@ class TestHootel(object):
     @allure.severity(allure.severity_level.TRIVIAL)
     @allure.tag("login")
     def test_login(self, email, password):
-        login_btn = self.browser.find_element(By.XPATH, '//a[@class="nav-link"]')
+            def test_login(self, email, password):
+        menu_toggle = WebDriverWait(self.browser, 5).until(
+            EC.element_to_be_clickable((By.XPATH, '//button[@class="navbar-toggler collapsed"]')))
+        menu_toggle.click()
+
+        login_btn = WebDriverWait(self.browser, 5).until(
+            EC.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
         login_btn.click()
 
         email_input = self.browser.find_element(By.ID, 'email')
@@ -36,11 +44,7 @@ class TestHootel(object):
 
         submit_btn = self.browser.find_element(By.NAME, 'submit')
         submit_btn.click()
-        time.sleep(1)
 
-        logout_btn = self.browser.find_element(By.ID, 'logout-link')
-
-        assert logout_btn.text == "Kilépés"
 
     def test_hotel_list(self):
         hotel_list_btn = self.browser.find_element(By.XPATH, '//button[@class="btn btn-outline-primary btn-block"]')
